64aaac7586d190aca90e85afb6a46475a6c2b2d2
Tomasz Nocoń <tomasz.nocon@hotmail.com>
2018-03-13 00:17:48 +0100

    Make the code work

--- core/src/main/scala/com/nocotom/ms/FilterSongs.scala
+++ core/src/main/scala/com/nocotom/ms/FilterSongs.scala
@@ -1,14 +1,14 @@
 package com.nocotom.ms
 
 import com.nocotom.ms.input.SongsSource
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+
 import scala.concurrent.duration._
 
-class FilterSongs {
+object FilterSongs {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     val dataStream = env.addSource(new SongsSource(10, 4.seconds))
-                        .filter(_ => true)
 
     dataStream.print()
 
--- core/src/main/scala/com/nocotom/ms/input/SongsSource.scala
+++ core/src/main/scala/com/nocotom/ms/input/SongsSource.scala
@@ -1,35 +1,44 @@
 package com.nocotom.ms.input
 
+import akka.actor._
 import com.nocotom.ms.model._
-import monix.execution.Cancelable
-import monix.execution.Scheduler.{global => scheduler}
 import org.apache.flink.streaming.api.functions.source.{RichParallelSourceFunction, SourceFunction}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.util.Random
 
 class SongsSource(numberOfUsers: Int, period: FiniteDuration) extends RichParallelSourceFunction[SongEvent] {
 
-  private var cancelable: Cancelable = _
+  private var running : Boolean = true
 
-  override def cancel(): Unit = cancelable.cancel()
+  override def cancel(): Unit = running = false
 
   override def run(sourceContext: SourceFunction.SourceContext[SongEvent]): Unit = {
-    val rnd = new Random
+    val random = new Random
+
+    val system = ActorSystem.create()
 
     // schedule worker at fixed rate
-    cancelable = scheduler.scheduleAtFixedRate(2.seconds, period) {
+    val cancellable = system.scheduler.schedule(2.seconds, period) {
 
       // random values
-      val userId = rnd.nextInt(numberOfUsers)
-      val songIdx = rnd.nextInt(Songs.SONGS.length)
-      val eventTypeIdx = rnd.nextInt(SongEventType.values.size)
+      val userId = random.nextInt(numberOfUsers)
+      val songIndex = random.nextInt(Songs.SONGS.length)
+      val eventTypeIndex = random.nextInt(SongEventType.values.size)
 
       // create song event
-      val songEvent = new SongEvent(Songs.SONGS(songIdx), System.currentTimeMillis, SongEventType(eventTypeIdx), userId)
+      val songEvent = new SongEvent(Songs.SONGS(songIndex), System.currentTimeMillis, SongEventType(eventTypeIndex), userId)
 
       // publish event
       sourceContext.collect(songEvent)
     }
+
+    while (running) {
+      Thread.sleep(10L)
+    }
+
+    cancellable.cancel()
+    system.terminate()
   }
 }
